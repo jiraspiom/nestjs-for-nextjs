28af977dcb0f7d9a52015b473073976f1f592f9e
1gilmar <1gilmar@gmail.com>
2026-06-26 15:36:30 -0300

    error

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3003);
 }
-bootstrap();
+// adicionado o "void" para evitar o erro "Promise<void> is not assignable to type 'void'." no TypeScript 6.0.3
+void bootstrap();
